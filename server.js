253af7d35a9a0a5ef9d62444ff3b0df406db7a39
bharathgoud2510 <Bharathgoud1143@gmail.com>
2026-03-20 14:57:46 +0530

    Update server.js

--- server.js
+++ server.js
@@ -21,5 +21,5 @@ app.get("/api/admissions", (req, res) => {
   res.json(enquiries);
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => console.log("Server running"));
